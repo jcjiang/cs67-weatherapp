f30cf6960c11adda66dc4c5234385e297678b319
Acacia Hoisington <acacia.d.hoisington.18@dartmouth.edu>
2018-05-16 14:01:01 -0400

    fixed daily expanded pages

--- app/src/main/java/com/dartmouth/cs/weatherapp/WeatherHomeFragment.java
+++ app/src/main/java/com/dartmouth/cs/weatherapp/WeatherHomeFragment.java
@@ -20,6 +20,7 @@ public class WeatherHomeFragment extends Fragment {
     private ImageButton daily;
     private ImageButton add_button;
     private Boolean hourly_expanded = false;
+    private Boolean daily_expanded = false;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_weatherhome, container, false);
@@ -31,14 +32,24 @@ public class WeatherHomeFragment extends Fragment {
         daily.setImageResource(R.drawable.main_daily_hanover);
         daily.setAdjustViewBounds(true);
 
-//        ImageView daily=(ImageView) rootView.findViewById(R.id.daily); // image
-//        daily.setImageResource(R.drawable.main_daily_hanover);
+        daily.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (!daily_expanded){
+                    daily.setImageResource(R.drawable.main_daily_expanded_hanover);
+
+                } else{
+                    daily.setImageResource(R.drawable.main_daily_hanover);
+                }
+                daily.setAdjustViewBounds(true);
+                daily_expanded=!daily_expanded;
+            }
+        });
 
         ImageView sun=(ImageView) rootView.findViewById(R.id.sun); // image
         sun.setImageResource(R.drawable.main_sun);
 
-//        ImageView hourly=(ImageView) rootView.findViewById(R.id.hourly); //image
-//        hourly.setImageResource(R.drawable.main_hourly_hanover);
         hourly=(ImageButton) rootView.findViewById(R.id.hourly); //image
         hourly.setImageResource(R.drawable.main_hourly_hanover);
         hourly.setAdjustViewBounds(true);
@@ -71,11 +82,7 @@ public class WeatherHomeFragment extends Fragment {
 
         });
 
-
-
         return rootView;
     }
-
-
 
 }
