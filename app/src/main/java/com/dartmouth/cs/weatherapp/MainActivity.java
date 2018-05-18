6fe55c25195fcf854ecf9ab8890db1d7aef483ec
savannah <savannah.m.liu.18@dartmouth.edu>
2018-05-18 00:53:37 -0400

    thank the lord it works added shared prefs so if u add tiles to one location then to another they are all saved

--- app/src/main/java/com/dartmouth/cs/weatherapp/MainActivity.java
+++ app/src/main/java/com/dartmouth/cs/weatherapp/MainActivity.java
@@ -7,42 +7,82 @@ import android.os.Bundle;
 import android.util.Log;
 import android.support.v4.app.FragmentManager;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import android.content.SharedPreferences;
+import android.widget.Toast;
 
-import android.support.v4.app.FragmentTransaction;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 
 public class MainActivity extends AppCompatActivity {
 
     public ViewPager ViewPager;
     public ArrayList<Fragment> fragments; // used to be private. need public to add tiles
-
     public SlidingTabLayoutWeather slidingTabLayoutWeather;
     public ViewPageAdapter ViewPageAdapter; // used to be private. need public to add tiles
+
     public HashMap bostonAddedTiles;
     public HashMap homeAddedTiles; // aka hanover
-
     public String sendTo;
+    public static String SHARED_PREF = "my_sharedpref"; // shared preferences
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("main activity", "oncreate");
+
+        // retrieve from shared prefs
+        //get from shared prefs
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREF, MODE_PRIVATE);
+
         setContentView(R.layout.activity_main);
 
         Bundle bundle = this.getIntent().getExtras();
 
-        if(bundle != null) {
-            HashMap checkedHashMap = (HashMap) bundle.getSerializable("checkedHashmap");
-            Log.d("hashmap mainactivity", checkedHashMap.toString());
-            sendTo = (String) bundle.getString("SEND_TO");
-            Log.d("sendTo mainactivity", sendTo);
-            if (sendTo.equals("hanover")){
-                homeAddedTiles = checkedHashMap;
-            } else if (sendTo.equals("boston")) {
-                bostonAddedTiles = checkedHashMap;
-            }
+        String homeMapString = prefs.getString("hanovertiles", "default");
+        java.lang.reflect.Type type = new TypeToken<HashMap<String, String>>(){}.getType();
+        Gson gson = new Gson();
+        homeAddedTiles = gson.fromJson(homeMapString, type);
+        Log.d("homeAddedTiles", "");
 
-        }
+        String bostonMapString = prefs.getString("bostontiles", "default");
+        java.lang.reflect.Type type2 = new TypeToken<HashMap<String, String>>(){}.getType();
+        Gson gson2 = new Gson();
+        bostonAddedTiles = gson2.fromJson(bostonMapString, type2);
+        Log.d("bostonAddedTiles","");
+
+
+//        if(bundle != null) {
+//            // HashMap checkedHashMap = (HashMap) bundle.getSerializable("checkedHashmap");
+//            // Log.d("hashmap mainactivity", checkedHashMap.toString());
+//            sendTo = (String) bundle.getString("SEND_TO");
+//
+//            Log.d("sendTo mainactivity", sendTo);
+//            if (sendTo.equals("hanover")){
+//                String storedHashMapString = prefs.getString("hanovertiles", "default");
+//                java.lang.reflect.Type type = new TypeToken<HashMap<String, String>>(){}.getType();
+//                Gson gson = new Gson();
+//                homeAddedTiles = gson.fromJson(storedHashMapString, type);
+//                Log.d("homeAddedTiles", "");
+//                // homeAddedTiles = checkedHashMap;
+//
+//            } else if (sendTo.equals("boston")) {
+//                String storedHashMapString = prefs.getString("bostontiles", "default");
+//                java.lang.reflect.Type type = new TypeToken<HashMap<String, String>>(){}.getType();
+//                Gson gson = new Gson();
+//                bostonAddedTiles = gson.fromJson(storedHashMapString, type);
+//                Log.d("bostonAddedTiles","");
+//
+//               //  bostonAddedTiles = checkedHashMap;
+//            }
+//
+//        }
 
         ViewPager = (ViewPager) findViewById(R.id.viewpager);
         slidingTabLayoutWeather = (SlidingTabLayoutWeather) findViewById(R.id.tab);
--- app/src/main/java/com/dartmouth/cs/weatherapp/WeatherBostonFragment.java
+++ app/src/main/java/com/dartmouth/cs/weatherapp/WeatherBostonFragment.java
@@ -2,6 +2,7 @@ package com.dartmouth.cs.weatherapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,8 @@ import android.widget.ImageView;
 import android.widget.ImageButton;
 import android.view.View.OnClickListener;
 import android.content.Intent;
+
+import java.util.HashMap;
 
 /**
  * Created by acaciah on 5/11/18.
@@ -22,12 +25,17 @@ public class WeatherBostonFragment extends Fragment {
     private Boolean hourly_expanded = false;
     private Boolean daily_expanded = false;
 
-    // private Boolean clothing_added;
-    // private Boolean radar_added;
-    // private Boolean air_added;
+    private ImageView clothing, radar, air;
+    private Boolean radar_expanded = false;
+    private HashMap addedTiles;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_weatherboston, container, false);
+
+        // get the added tiles from main activity
+        MainActivity activity = (MainActivity) getActivity();
+        addedTiles = activity.bostonAddedTiles;
 
         ImageView title=(ImageView) rootView.findViewById(R.id.title); // image
         title.setImageResource(R.drawable.main_title_boston);
@@ -51,12 +59,6 @@ public class WeatherBostonFragment extends Fragment {
             }
         });
 
-//        ImageView daily=(ImageView) rootView.findViewById(R.id.daily); // image
-//        daily.setImageResource(R.drawable.main_daily_boston);
-
-//        ImageView hourly=(ImageView) rootView.findViewById(R.id.hourly); //image
-//        hourly.setImageResource(R.drawable.main_hourly_boston);
-
         ImageView sun=(ImageView) rootView.findViewById(R.id.sun); // image
         sun.setImageResource(R.drawable.main_sun);
 
@@ -78,6 +80,7 @@ public class WeatherBostonFragment extends Fragment {
             }
         });
 
+
         add_button = (ImageButton) rootView.findViewById(R.id.add_button);
 
         add_button.setOnClickListener(new OnClickListener() {
@@ -85,10 +88,56 @@ public class WeatherBostonFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(view.getContext(), AddTilesActivity.class);
+                i.putExtra("SENT_FROM", "boston");
                 startActivity(i);
             }
 
         });
+
+        clothing = rootView.findViewById(R.id.boston_clothing); //image
+        radar = rootView.findViewById(R.id.boston_radar); //image
+        air = rootView.findViewById(R.id.boston_air); //image
+
+
+        if (addedTiles != null) {
+            // add custom tiles
+            if (addedTiles.containsKey("clothing")) {
+                Log.d("weatherbostonfrag", "clothing");
+                clothing.setImageResource(R.drawable.main_clothing);
+                clothing.setAdjustViewBounds(true);
+            }
+            if (addedTiles.containsKey("radar")) {
+                Log.d("weatherbostonfrag", "radar");
+                radar.setImageResource(R.drawable.main_radar);
+                radar.setAdjustViewBounds(true);
+                radar.setOnClickListener(new OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        Log.d("", "radar onclick");
+                        if (!radar_expanded) {
+                            Log.d("", "radar onclick not expanded");
+                            radar.setImageResource(R.drawable.main_radar_expanded);
+                        } else {
+                            Log.d("", "radar onclick expanded");
+                            radar.setImageResource(R.drawable.main_radar);
+                        }
+                        radar.setAdjustViewBounds(true);
+                        radar_expanded = !radar_expanded;
+                    }
+
+                });
+
+
+            }
+            if (addedTiles.containsKey("air")) {
+                Log.d("weatherbostonfrag", "air");
+                air.setImageResource(R.drawable.main_air);
+                air.setAdjustViewBounds(true);
+
+            }
+        }
+
 
         return rootView;
     }
